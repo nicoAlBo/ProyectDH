56b05ab465c6e7ad836eca9e6f091db484032356
GIT_020#19 <christian_090@hotmail.com>
2018-05-29 13:57:12 -0300

    Mensaje de bienvenida generado

--- ProyectoDigitalHouse-master/perfil.php
+++ ProyectoDigitalHouse-master/perfil.php
@@ -2,19 +2,25 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/style.css">
     <title></title>
   </head>
   <body>
-      <h1>registro OK</h1>
-
+    <div class="contenedor-bienvenida">
       <?php
     session_start();
 
-    echo 'Bienvenido '.$_SESSION['usuario'];
+    echo '<p class="mensaje-bienvenida">Bienvenido '.$_SESSION['usuario'] . '</p>';
 
      session_destroy();
        ?>
 
-       ahora  <a href="login.php">logeate</a>
+       <h3 class="registrado">Su usuario fue registrado</h3>
+
+       <p class="mensaje-bienvenida">Ahora podés loguearte <a href="login.php">aquí</a></p>
+
+       </div>
   </body>
 </html>
